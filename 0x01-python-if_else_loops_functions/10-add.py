2b016282d0e61d112d52aa67767c6658ccd1e6e0
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-11-30 22:41:19 +0000

    if else loops

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-a = 12, b = 15:
-value = a + b:
-print (value:d)
+"""Return the addition of a and b."""
+	return (a + b)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 #Author - Angel Oghenefuafo
-str = "My Name is Ovie"
-if str >= 97 && <= 122:
-	return True
-else:
-	return False
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
