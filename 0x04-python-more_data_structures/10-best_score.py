cc00e512dc7d3586e882ba7081bd187a1fc857b4
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-12-08 09:45:48 +0000

    list map

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-# 10-best_score.py
-
 
 def best_score(a_dictionary):
-    """return a key with big int value."""
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
-
-    lst = list(a_dictionary.keys())[0]
-    bg = a_dictionary[lst]
-    for j, k in a_dictionary.items():
-        if v > bg:
-            bg = j
-            lst = j
-    return (lst)
+    biggest = 0
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+    for key, value in a_dictionary.items():
+        if value == biggest:
+            return key
